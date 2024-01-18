edb8303d68f2a6472da6735e4f9c7c8ab0fc77b7
Nguyen huu phu <phub3@gmail.com>
2024-01-18 17:30:04 +0700

    remote-commit

--- project/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './component/home/home.component';
-import { LoginComponent } from './component/login/login.component';
-export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'login', component: LoginComponent },
-];
